9ddbed8de1e89f660e3aafc2a1eee959d0a4f0f4
Thor Summerall <thor.summerall@gmail.com>
2020-12-03 22:39:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,7 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count += 1
-      else
-        genre_count[genre] = 1
-      end
+      genre_count[genre] ? genre_count[genre] += 1 : genre_count[genre] = 1
     end
     genre_count
   end
