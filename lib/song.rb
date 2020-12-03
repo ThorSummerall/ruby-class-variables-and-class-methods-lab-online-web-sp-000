c597f940e029596d9ca891c5d03e058f2a0f2786
Thor Summerall <thor.summerall@gmail.com>
2020-12-03 22:26:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,16 @@
 class Song
-
   attr_accessor = :name, :artist, :genre
+  
   @@count = 0
   @@genres = []
   @@artists = []
 
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+  end
 end
